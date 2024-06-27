c6fef3bbe2d443c03057949e612dd870858e26e7
Hardik Jain <hardikjain0029@gmail.com>
2024-06-28 00:49:14 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -4,9 +4,21 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export { app }
+//rotues import 
+import userRouter from "./routes/user.routes.js";
+
+
+//rotues declartion
+app.use("/api/v1/users", userRouter)
+
+export { app }  
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({ 
-    path: "./env" 
+    path: "./.env" 
 })
 
 
 
 connectDB()
 .then(()=>{
-    app.on((err)=>{
-        console.log("ERROR 404 check the network !!",err);
-    })
     app.listen(process.env.PORT || 8000, ()=>{
         console.log(` Server is running at port: ${process.env.PORT}`);
     })
